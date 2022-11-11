55d43863ac9fc321a1009cdf7b36c0cfd9d96123
phuonglinh99vn <83125820+phuonglinh99vn@users.noreply.github.com>
2022-11-12 09:14:01 +1100

    Update MinimumGap.java

--- HW10/MinimumGap.java
+++ HW10/MinimumGap.java
@@ -10,7 +10,7 @@ public class MinimumGap {
 	public static void main(String[] args) {
 		int[] array = new int[] { 7, 5, 4, 7, 6, 7, 5 };
 		
-//		int minGap = array.length;
+//On^2		int minGap = array.length;
 //		for (int i = 0; i < array.length; i++) {
 //			for (int j = i + 1; j < array.length; j++) {
 //				if (array[i] == array[j] && minGap > j - i) {
@@ -18,7 +18,7 @@ public class MinimumGap {
 // 					break;
 //				}
 //			}
-//				
+//O(n)				
 		int[] arrGap = new int[maxValue(array)+1];
 		int[] index = new int[maxValue(array)+1];		
 		for (int j = 0; j < arrGap.length; j++) {
