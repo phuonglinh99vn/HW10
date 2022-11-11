b9e09951a945182fa3390a3c4001aa8eda02e845
phuonglinh99vn <83125820+phuonglinh99vn@users.noreply.github.com>
2022-11-12 09:12:45 +1100

    Add files via upload

--- HW10/MinimumGap.java
+++ HW10/MinimumGap.java
@@ -4,24 +4,27 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MinimumGap {
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		int[] array = new int[] { 7, 5, 4, 7, 7, 5 };
+		int[] array = new int[] { 7, 5, 4, 7, 6, 7, 5 };
 		
-// ON^2
+//		int minGap = array.length;
 //		for (int i = 0; i < array.length; i++) {
 //			for (int j = i + 1; j < array.length; j++) {
 //				if (array[i] == array[j] && minGap > j - i) {
 //					minGap = j - i;
-//					break;
+// 					break;
 //				}
 //			}
-//0(N)				
-		
-		int[] arrGap = new int[10000];
-		int[] index = new int[10000];		
+//				
+		int[] arrGap = new int[maxValue(array)+1];
+		int[] index = new int[maxValue(array)+1];		
 		for (int j = 0; j < arrGap.length; j++) {
 			arrGap[j] = array.length;
 		}	
+	
 		for (int i = 0; i < array.length; i++) {
 			if (index[array[i]] != 0) {
 				arrGap[array[i]] = Math.min(arrGap[array[i]], i - index[array[i]] + 1);
@@ -32,14 +35,22 @@ public class MinimumGap {
 	}	
 	public static int minGap(int[] arrGap) {
 		int minGap = 10000;
-		for (int i : arrGap) {
-			if (i > 0 && minGap > i) {
-				minGap = i;
+		for (int a : arrGap) {
+			if (a > 0 && minGap > a) {
+				minGap = a;
 			}
 		}
 		return minGap;
 	}
 	
-	
+	public static int maxValue(int[] array) {
+		int maxValue = array[0];
+		for (int a : array) {
+			if (maxValue < a) {
+				maxValue = a;
+			}
+		}
+		return maxValue;
+	}
 }
 
